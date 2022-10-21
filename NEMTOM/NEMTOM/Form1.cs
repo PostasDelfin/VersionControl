fed7d0cb212fe24636d2b1ed3f2d520f61411cc7
f9y613 <maximus02002@gmail.com>
2022-10-21 14:18:09 +0200

    entities

--- NEMTOM/NEMTOM/Form1.cs
+++ NEMTOM/NEMTOM/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NEMTOM.ServiceReference1;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,25 @@ namespace NEMTOM
 {
     public partial class Form1 : Form
     {
+        BindingList<Entities.RateData> Rates = new BindingList<Entities.RateData>();
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = Rates;
+        }
+        public void fv()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
